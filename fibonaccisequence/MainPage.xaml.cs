5ae25548df6439d6e1a1acfe21a1c74b7c361fe6
kaga <kaga21@gmail.com>
2021-07-16 04:45:21 +1200

    init branch

---
+++ fibonaccisequence/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace fibonaccisequence
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class NumberGeneratorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand MyCommand { private set; get; }
+
+        string numberLabelText;
+        public string NumberLabelText
+        {
+            set
+            {
+                if (numberLabelText != value)
+                {
+                    numberLabelText = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NumberLabelText"));
+                }
+            }
+            get
+            {
+                return numberLabelText;
+            }
+        }
+
+        public NumberGeneratorViewModel()
+        {
+            MyCommand = new Command(execute: () =>
+            {
+                generateNextNumber();
+            });
+            NumberLabelText = "-";
+        }
+
+        public void generateNextNumber()
+        {
+            NumberLabelText = "2";
+
+        }
+    }
+}
